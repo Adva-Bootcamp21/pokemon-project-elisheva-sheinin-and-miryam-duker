dbef82a4afa2dcef3e52cb58fe573a412ff47bde
elishevasheinin <elishevasheinin@gmail.com>
2021-07-06 16:06:07 +0300

    without comments

--- pokemon.sql
+++ pokemon.sql
@@ -35,23 +35,3 @@ CREATE TABLE has_types(
   FOREIGN KEY (type_id) REFERENCES types(id) ON DELETE CASCADE,
   PRIMARY KEY (pokemon_id, type_id)
 );
-
--- CREATE TABLE pokemon(
---   id INTEGER PRIMARY KEY NOT NULL,
---   name VARCHAR(25),
---   type VARCHAR (25),
---   height INTEGER,
---   weight INTEGER
--- );
--- CREATE TABLE trainer(
---   id INTEGER PRIMARY KEY,
---   name VARCHAR (25),
---   town VARCHAR (25)
--- );
--- CREATE TABLE owned_by(
---   pokemon_id INTEGER ,
---   trainer_id INTEGER ,
---   FOREIGN KEY (pokemon_id) REFERENCES pokemon(id),
---   FOREIGN KEY (trainer_id) REFERENCES trainer(id),
---   PRIMARY KEY (pokemon_id, trainer_id)
--- );
